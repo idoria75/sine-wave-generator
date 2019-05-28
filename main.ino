173517c4fb595fd8ef2807de5a72bb1344501910
= <=>
2019-05-27 22:42:02 -0300

    New branch

--- main.ino
+++ main.ino
@@ -1,27 +1,7 @@
-int ledPin = 9;
-byte count = 0;
-boolean flag = true;
-void setup() {  // initialize digital pin LED_BUILTIN as an output.
-  Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
+void setup() {
+  ;
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  if (flag) {
-    count++;
-  } else {
-    count--;
-  }
-  analogWrite(ledPin, count);
-  if (count == 254) {
-    flag = false;
-    Serial.println(flag);
-  }
-  if (count == 1) {
-    flag = true;
-    Serial.println(flag);
-  }
-  delay(5);
-  Serial.println(count);
+  ;
 }
